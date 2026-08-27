7471c74352abcec350114d81f274398cb451894f
wqfpjzm <wqfpjzm@gmail.com>
2026-08-27 14:57:40 +0300

    add env variabls

---
+++ services/auth-service/app/config.py
@@ -0,0 +1,12 @@
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):   
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    database_url: str 
+    jwt_secret: SecretStr
+    jwt_algorithm: str = "HS256"
+
+
+settings = Settings()
