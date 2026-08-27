acf494a065f66ae5bfcc8e7a52b1ac5f0a86df3f
wqfpjzm <wqfpjzm@gmail.com>
2026-08-27 15:23:05 +0300

    added user model for db

---
+++ services/auth-service/app/models.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from uuid import UUID, uuid4
+
+from sqlalchemy import DateTime, String, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    id: Mapped[str] = mapped_column(
+        String,
+        primary_key=True,
+        default=lambda: str(uuid4()),
+    )
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    email: Mapped[str] = mapped_column(unique=True)
+    hashed_password: Mapped[str]
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+    )
+
+
+
